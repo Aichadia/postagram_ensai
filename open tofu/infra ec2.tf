49c008765d2910cef009ed19aa35e26cab4c8146
Niang Seynabou <seynabou@MacBook-Air-de-Seynabou.local>
2026-04-17 19:43:54 +0200

    Automate EC2 webservice startup and env setup

--- open tofu/infra ec2.tf
+++ open tofu/infra ec2.tf
@@ -1,9 +1,8 @@
 # Aidez-vous du TP 2
 
-
 variable "git_repo" {
-  type = string
-  default = "https://github.com/Aichadia/postagram_ensai.git" # Url de notre dépôt git
+  type    = string
+  default = "https://github.com/Aichadia/postagram_ensai.git"
 }
 
 ########################################
@@ -12,19 +11,19 @@ variable "git_repo" {
 
 resource "aws_launch_template" "ubuntu_template" {
   name_prefix   = "postagram-"
-  image_id      = "ami-0ec10929233384c7f" # Ubuntu 24.04 LTS us-east-1 comme dans TP1 
+  image_id      = "ami-0ec10929233384c7f" # Ubuntu 24.04 LTS us-east-1
   instance_type = "t3.micro"
   key_name      = "vockey"
+
   iam_instance_profile {
-    #arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:instance-profile/LabRole" # <- NE PAS MODIFIER -- modif de l'arguement name en arn
     name = "LabInstanceProfile"
-    } 
+  }
 
   user_data = base64encode(templatefile("${path.module}/user_data.sh", {
-    git_repo = var.git_repo
+    git_repo     = var.git_repo
     dynamo_table = aws_dynamodb_table.basic-dynamodb-table.name
-    bucket = aws_s3_bucket.bucket.bucket
-    })) # ajout du base64 encoder 
+    bucket       = aws_s3_bucket.bucket.bucket
+  }))
 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
 
@@ -39,10 +38,12 @@ resource "aws_launch_template" "ubuntu_template" {
 
   tag_specifications {
     resource_type = "instance"
+
     tags = {
       Name = "postagram-instance"
     }
   }
+
   tags = {
     Name = "TP noté"
   }
@@ -51,29 +52,31 @@ resource "aws_launch_template" "ubuntu_template" {
 ########################################
 # Auto Scaling Group
 ########################################
+
 resource "aws_autoscaling_group" "web_asg" {
-  desired_capacity     = 1
-  max_size             = 4
-  min_size             = 1
-  vpc_zone_identifier  = data.aws_subnets.default.ids
-  health_check_type    = "EC2"
-  target_group_arns    = [aws_lb_target_group.web_tg.arn]
+  desired_capacity    = 1
+  max_size            = 4
+  min_size            = 1
+  vpc_zone_identifier = data.aws_subnets.default.ids
+  health_check_type   = "EC2"
+  target_group_arns   = [aws_lb_target_group.web_tg.arn]
 
   launch_template {
     id      = aws_launch_template.ubuntu_template.id
     version = "$Latest"
-    }
+  }
 
   tag {
     key                 = "Name"
     value               = "web-asg-instance"
     propagate_at_launch = true
-    }
   }
+}
 
 ########################################
 # Load Balancer (ALB)
 ########################################
+
 resource "aws_lb" "web_alb" {
   name               = "web-alb"
   internal           = false
@@ -83,27 +86,38 @@ resource "aws_lb" "web_alb" {
 
   tags = {
     Name = "web-alb"
-    }
   }
+}
 
 ########################################
-# Target Group (pour le Load Balancer)
+# Target Group
 ########################################
+
 resource "aws_lb_target_group" "web_tg" {
   name     = "web-tg"
   port     = 8080
   protocol = "HTTP"
   vpc_id   = data.aws_vpc.default.id
 
+  health_check {
+    path                = "/posts"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 
   tags = {
     Name = "web-tg"
-    }
   }
+}
 
 ########################################
 # Listener pour le Load Balancer
 ########################################
+
 resource "aws_lb_listener" "http_listener" {
   load_balancer_arn = aws_lb.web_alb.arn
   port              = 80
@@ -112,12 +126,13 @@ resource "aws_lb_listener" "http_listener" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.web_tg.arn
-    }
   }
+}
 
 ########################################
 # Règle Security Group port 8080
 ########################################
+
 resource "aws_security_group_rule" "allow_8080" {
   type              = "ingress"
   from_port         = 8080
@@ -130,8 +145,8 @@ resource "aws_security_group_rule" "allow_8080" {
 ########################################
 # Outputs
 ########################################
+
 output "load_balancer_dns_name" {
   description = "Nom DNS du load balancer"
   value       = aws_lb.web_alb.dns_name
 }
-
--- open tofu/user_data.sh
+++ open tofu/user_data.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-echo "userdata-start"        
+echo "userdata-start"
 set -e
+
 apt update
-apt install -y python3-pip python3.12-venv
+apt install -y git python3-pip python3.12-venv
+
 git clone ${git_repo} projet
 cd projet/webservice
+
 python3 -m venv venv
 source venv/bin/activate
-rm .env
-echo 'BUCKET=${bucket}' >> .env
-echo 'DYNAMO_TABLE=${dynamo_table}' >> .env
+
+rm -f .env
+echo "BUCKET=${bucket}" >> .env
+echo "DYNAMO_TABLE=${dynamo_table}" >> .env
+echo "AWS_DEFAULT_REGION=us-east-1" >> .env
+
 pip3 install -r requirements.txt
-venv/bin/python app.py
-echo "userdata-end
+
+nohup venv/bin/python app.py > /var/log/postagram.log 2>&1 &
+
+echo "userdata-end"
