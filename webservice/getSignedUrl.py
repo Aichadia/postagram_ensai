2cff3b220cd13945f33a9cd946ab2a3ab9a0f696
Niang Seynabou <seynabou@MacBook-Air-de-Seynabou.local>
2026-04-17 16:07:20 +0200

    Fix signed URL generation for S3 uploads

--- webservice/getSignedUrl.py
+++ webservice/getSignedUrl.py
@@ -4,7 +4,6 @@
 ##                                              ##
 ##################################################
 
-
 import logging
 import boto3
 from boto3.dynamodb.conditions import Key
@@ -14,11 +13,11 @@ import uuid
 from pathlib import Path
 from botocore.exceptions import ClientError
 
-bucket = os.getenv("BUCKET")
-s3_client = boto3.client('s3', config=boto3.session.Config(signature_version='s3v4'))
 logger = logging.getLogger("uvicorn")
 
-def getSignedUrl(filename: str,filetype: str, postId: str, user):
+def getSignedUrl(filename: str, filetype: str, postId: str, user):
+    bucket = os.getenv("BUCKET")
+    s3_client = boto3.client('s3', config=boto3.session.Config(signature_version='s3v4'))
 
     filename = f'{uuid.uuid4()}{Path(filename).name}'
     object_name = f"{user}/{postId}/{filename}"
@@ -26,18 +25,18 @@ def getSignedUrl(filename: str,filetype: str, postId: str, user):
     try:
         url = s3_client.generate_presigned_url(
             Params={
-            "Bucket": bucket,
-            "Key": object_name,
-            "ContentType": filetype
-        },
+                "Bucket": bucket,
+                "Key": object_name,
+                "ContentType": filetype
+            },
             ClientMethod='put_object'
         )
     except ClientError as e:
         logging.error(e)
-
+        raise e
 
     logger.info(f'Url: {url}')
     return {
-            "uploadURL": url,
-            "objectName" : object_name
-        }
+        "uploadURL": url,
+        "objectName": object_name
+    }
